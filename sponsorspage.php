711b1a2fbabb130511126884e9175460da21b396
carolinesbutler <55598499+carolinesbutler@users.noreply.github.com>
2020-03-20 11:23:13 -0600

    Update sponsorspage.php

--- sponsorspage.php
+++ sponsorspage.php
@@ -3,53 +3,45 @@
 <html>
 <head>
   <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=.7, minimum-scale=.7, maximum-scale=.7">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=.7, minimum-scale=.7, maximum-scale=.7">
   <title>oneabq sponsors</title>
   <PHP><link rel="stylesheet" type="text/css" href="sponsorspage.css"><PHP>
   <link rel="stylesheet" href="sponsorspage.css?v=<?php echo time(); ?>">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"></script>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
-  <body>
-        <div class="navBar">
-                <header>
-                  <nav> 
-                    <div class="menu-icon">
-                      <i class="fa fa-bars fa-2x"></i>
-                    </div>
-                    <div class="oneabqlogo">
-                      <a href="oneabqhome.php"><img src="abqhoriz.png" class="logo"></a>
-                    </div>
-                   <div class="menu">
-                     <ul>
-                        <li><a href="aboutabq.php" style=font-size:15px;>About</a></li>
-                        <li><a href="priorities.php" style=font-size:15px;>Priorities</a></li>
-                        <li class="activeNav"><a href="sponsorspage.php"style=font-size:15px><font color="red";>Sponsors</font></a></li>
-                        <li><a href="outcomes.php" style=font-size:15px>Outcomes</a></li>
-                        <a href="donate.php"><button class="buttonDonate">Donate</button></a>
-                        <li><a href="contactpage.php" style=font-size:15px>Contact</a></li>
-                       
-                        
-                     </ul>
-                    </div>
-                  </nav>
-                </header>
-              </div>
-
-              <div class="sponsorSplash">
-
-                    <h1 class="sponsHead"><b>Community Partners</b></h1>
-                    <p class="splashText">Thank you to our partners in the community who are<br>
-                      stepping up to help meet Albuquerque’s challenges<br>
-                      head-on.<br></p>
-
-            
+<body>
+  <div class="navBar">
+    <header>
+      <nav> 
+        <div class="menu-icon">
+          <i class="fa fa-bars fa-2x"></i>
+        </div>
+            <div class="oneabqlogo">
+              <a href="oneabqhome.php"><img src="abqhoriz.png" class="logo"></a>
+            </div>
+                <div class="menu">
+                  <ul>
+                    <li><a href="aboutabq.php" style=font-size:15px;>About</a></li>
+                    <li><a href="priorities.php" style=font-size:15px;>Priorities</a></li>
+                    <li class="activeNav"><a href="sponsorspage.php"style=font-size:15px><font color="red";>Sponsors</font></a></li>
+                    <li><a href="outcomes.php" style=font-size:15px>Outcomes</a></li>
+                    <a href="donate.php"><button class="buttonDonate">Donate</button></a>
+                    <li><a href="contactpage.php" style=font-size:15px>Contact</a></li>
+                  </ul>
                 </div>
-
-                <div class="sponsorLogos">
+        </nav>
+    </header>
+  </div>
+  <div class="sponsorSplash">
+    <h1 class="sponsHead"><b>Community Partners</b></h1>
+      <p class="splashText">Thank you to our partners in the community who are<br>
+                            stepping up to help meet Albuquerque’s challenges<br>
+                            head-on.<br></p>
+  </div>
+  <div class="sponsorLogos">
                    <center>
                     <img src="allensigmon.png" id="logo">
                     <img src="bcbs.png" id="logo">
@@ -79,50 +71,42 @@
                 </div>
 
               
-                <div class="footer">
-
-<div style="border-left: solid black; height: 81.5%; margin-left: -10px;">
-
-
-<div class="row">
-<div class="col-md-4">
-
-<div class="contactUs">
-
-<p><b>CONTACT US</b></p>
-</div>
-<div class="contactUs2">
-<p style="line-height:1.2"><b>Steve Fitzer, Development Director<br></b>
-+1.505.633.6813<br>
-stevefitzer@onealbuquerque.org<br>
-PO BOX 25125<br>
-Albuquerque NM 87125-5125</p>
-</div>
-</div>
-
-<div class="col-md-4 , footerLogo" >
-<img src="abqhoriz2.png" class="footerLogo"> 
-</div>
-<div class="col-md-4 , links">
-<a href="oneabqhome.html"><p><b>Home</b></p></a>
-<p><b>About</b></p>
-<a href="priorities.php"><p><b>Priorities</b></p></a>
-<a href="contactpage.php"><p><b>Sponsors</b></p></a>
-<a href="contactpage.php"><p><b>Outcomes</b></p></a>
-<a href="donate.php"><p><b>Donate</b></p></a>
-<a href="contactpage.php"><p><b>Contact</b></p></a>
-</div>
-</div>
-<div class="row">
-<div class="col-md-2"></div>
-<div class="col-md-8 , rights">
-<p>2019 City of Albuquerque Foundation| All Rights Reserved | The offical website for the One Albuquerque Fund</p>
-</div> 
-<div class="col-md-2"></div>
-</div>
-</div>
-
-<script src="oneabq.js"></script>
-  </body>
-
-  </html>
+  <div class="footer">
+    <div style="border-left: solid black; height: 81.5%; margin-left: -10px;">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="contactUs">
+            <p><b>CONTACT US</b></p>
+          </div>
+  <div class="contactUs2">
+    <p style="line-height:1.2"><b>Steve Fitzer, Development Director<br></b>
+    +1.505.633.6813<br>
+    stevefitzer@onealbuquerque.org<br>
+    PO BOX 25125<br>
+    Albuquerque NM 87125-5125</p>
+  </div>
+  </div>
+  <div class="col-md-4 , footerLogo" >
+    <img src="abqhoriz2.png" class="footerLogo"> 
+  </div>
+  <div class="col-md-4 , links">
+    <a href="oneabqhome.html"><p><b>Home</b></p></a>
+    <p><b>About</b></p>
+    <a href="priorities.php"><p><b>Priorities</b></p></a>
+    <a href="contactpage.php"><p><b>Sponsors</b></p></a>
+    <a href="contactpage.php"><p><b>Outcomes</b></p></a>
+    <a href="donate.php"><p><b>Donate</b></p></a>
+    <a href="contactpage.php"><p><b>Contact</b></p></a>
+  </div>
+        </div>
+  <div class="row">
+    <div class="col-md-2"></div>
+      <div class="col-md-8 , rights">
+        <p>2019 City of Albuquerque Foundation| All Rights Reserved | The offical website for the One Albuquerque Fund</p>
+      </div> 
+  <div class="col-md-2"></div>
+  </div>
+  </div>
+  <script src="oneabq.js"></script>
+</body>
+</html>
