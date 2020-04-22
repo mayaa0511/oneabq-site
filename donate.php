0a77ec0caa8767d647939b2b90cf38beb6fe46df
carolinesbutler <55598499+carolinesbutler@users.noreply.github.com>
2020-04-22 14:13:53 -0600

    Update donate.php

--- donate.php
+++ donate.php
@@ -10,60 +10,90 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"></script>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="donate.css?v=<?php echo time(); ?>">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="donate.css?v=<?php echo time(); ?>">
 </head>
   <body>
-<?php include("header.html")?>
-  <div class="donateOptions">
-  <div class="row">
-  <div class="col-sm-3">
-  <div class="donateIcon">
-    <img src="police-icon.png" height="80px" width="80px"> 
-  </div>
-  </div>
-  <div class="col-sm-3">
-    <h2><b>Officer<br>Recruitment</b></h2>
-      <p>Your donation to the officer recruitment
-        fund will help the Albuquerque Police
-        Department recruit more officers to make our city safer.</p>
-  </div>
-  <div class="col-sm-3">
-    <img class="donateIcon2" src="school-icon.png" height="80px" width="80px">
-      <h2><b>Youth<br>Opportunity</b></h2>
-      <p>Your donation to the Youth Opportunity Fund will provide scaling funds to fill out, and increase, opportunities at existing programs that serve young people in Albuquerque.</p>
-  </div>
-  <div class="col-sm-3">
-  </div>
-  </div>
-  <div class="donateOptions">
+    <div class="navBar">
+      <header>
+
+        <nav> 
+          <div class="menu-icon">
+            <i class="fa fa-bars fa-2x"></i>
+          </div>
+
+
+          <div class="oneabqlogo">
+            <a href="oneabqhome.php"><img src="abqhoriz.png" class="logo"></a>
+          </div>
+         <div class="menu">
+           <ul>
+              <li><a href="aboutabq.php" style=font-size:15px;>About</a></li>
+              <li><a href="priorities.php" style=font-size:15px;>Initiatives</a></li>
+              <li><a href="sponsorspage.php" style=font-size:15px;>Sponsors</a></li>
+              <li><a href="outcomes.php" style=font-size:15px;>Outcomes</a></li>
+			  <li><a href="contactpage.php" style=font-size:15px;>Contact</a></li>
+              <button class="buttonDonate"><b><font color="red";>Donate</font></b></button>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </div>
+
+<div class="donateOptions">
+    <div class="row">
+      <div class="col-sm-3">
+        <div class="donateIcon">
+      
+    </div>
+        </div>
+        <div class="col-sm-3">
+        	<img src="police-icon.png" height="80px" width="80px"> 
+            <h2><b>Officer<br>Recruitment</b></h2>
+            <p>Your donation to the officer recruitment
+            fund will help the Albuquerque Police
+            Department recruit more officers to make our city safer.</p>
+        </div>
+        
+        <div class="col-sm-3">
+        	<img src="school-icon.png" height="80px" width="80px">
+            <h2><b>Youth<br>Opportunity</b></h2>
+            <p>Your donation to the Youth Opportunity Fund will provide scaling funds to fill out, and increase, opportunities at existing programs that serve young people in Albuquerque.</p>
+        </div>
+        <div class="col-sm-3">
+        </div>
+    </div>
     <div class="row">
         <div class="col-sm-3"> 
-          <div class="donateIcon">
-            <img src="work-icon.png" height="80px" width="80px">
-          </div>
+          <div class="workIcon">
+          
         </div>
-    <div class="col-sm-3">
-      <h2><b>Workforce<br>Support</b></h2>
-          <p>Your donation to the Workforce Training fund will help strengthen the newly created Job Training Albuquerque program.</p>
+        </div>
+        <div class="col-sm-3">
+        <img src="work-icon.png" height="80px" width="80px">
+                    <h2><b>Workforce<br>Training</b></h2>
+                    <p>Your donation to the Workforce Training fund will help strengthen the newly created Job Training Albuquerque program.</p>
+        </div>
+        
+        <div class="col-sm-3">
+        <img src="handhouse-icon.png" height="80px" width="80px">
+                    <h2><b>Homelessness<br>& Housing</b></h2>
+                    <p>Your donation to the Housing Voucher fund will support a “housing first” model of tackling homelessness in Albuqeurque.</p>
+        </div>
+        <div class="col-sm-3">
+        </div>
     </div>
-  <div class="col-sm-3">
-    <img class="donateIcon2" src="handhouse-icon.png" height="80px" width="80px">
-      <h2><b>Homelessness &<br>Housing</b></h2>
-        <p>Your donation to the Housing Voucher fund will support a “housing first” model of tackling homelessness in Albuqeurque.</p>
-  </div>
-  <div class="col-sm-3">
   </div>
   </div>
-  </div>
-  </div>
-  </div>
+</div>
   <center><div id="cpa1989e5448" style="margin: 37px 0px" class="rounded-div" onclick="document.getElementById('pay_now_form_d66ec123ef').submit()"><form action="https://checkout.globalgatewaye4.firstdata.com/pay" id="pay_now_form_d66ec123ef" method="post"><input type="hidden" name="x_login" value="WSP-ONE-A-PjgeagDGNw" /><input type="hidden" name="x_show_form" value="PAYMENT_FORM" /><input type="hidden" name="x_fp_sequence" value="15738680973699013222" /><input type="hidden" name="x_fp_hash" value="PNB-1.0-b74016462aa610f6cb8f20a75da91c090ea3cd77" /><input type="hidden" name="x_amount" /><input type="hidden" name="x_currency_code" value="USD" /><input type="hidden" name="x_test_request" value="FALSE" /><input type="hidden" name="x_relay_response" value="" /><input type="hidden" name="donation_prompt" /><input type="hidden" name="button_code" value="Pay Now One Albuquerque Fund" /><button type="button">Donate</button></div></form></div></center>
+
 
 <style type="text/css">div#cpa1989e5448{margin-top:10px;width: 115px; background-color: #CC3333;border-radius: 8px;  padding: 5px 0;}div#cpa1989e5448:hover{cursor: pointer}div#cpa1989e5448 form{margin:0; padding:0;text-align:center}div#cpa1989e5448 button{width: 95%;border:0; margin:0 auto;padding: 3px 0; background-color: #CC3333;text-align: center; color: #FFFFFF; }div#cpa1989e5448:hover button {text-decoration: none}div#cpa1989e5448 button:focus,div#cpa1989e5448 button:visited,div#cpa1989e5448 button:active{border:none;outline: none; align-items: center;}div#cpa1989e5448 button {font-size: 13px}div#cpa1989e5448{background-color: #CC3333;}</style>
 </div>
-  </div>
-  <div class="footer container-fluid">
+
+
+<div class="footer container-fluid">
     <div style="border-left: solid black; height: 81.5%; margin-left: -10px;">
       <div class="row">
         <div class="col-md-4">
